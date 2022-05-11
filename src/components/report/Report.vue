ace4f22477508d0a6157f7b293b1e41a76d841ac
fengzhe <29698401844@qq.com>
2022-05-11 14:59:13 +0800

    xiugai

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -7,24 +7,24 @@
       <el-breadcrumb-item>数据报表</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-      <div id="main" style="width: 600px;height:400px;"></div>
+      <div id="main" style="width: 600px;height:400px;">服务器异常</div>
     </el-card>
   </div>
 </template>
 <script>
-import echarts from 'echarts'
+// import echarts from 'echarts'
 export default {
-  name: 'ReportSer',
-  async mounted () {
-  // 基于准备好的dom，初始化echarts实例
-    const myChart = echarts.init(document.getElementById('main'))
-    // 指定图表的配置项和数据
-    const { data: res } = await this.$http.get('reports/type/1')
-    if (res.meta.status !== 200) return this.$message.error('数据获取失败')
-    console.log(res.data)
-    // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(res.data)
-  }
+  // name: 'ReportSer',
+  // async mounted () {
+  // // 基于准备好的dom，初始化echarts实例
+  //   const myChart = echarts.init(document.getElementById('main'))
+  //   // 指定图表的配置项和数据
+  //   const { data: res } = await this.$http.get('reports/type/1')
+  //   if (res.meta.status !== 200) return this.$message.error('数据获取失败')
+  //   console.log(res.data)
+  //   // 使用刚指定的配置项和数据显示图表。
+  //   myChart.setOption(res.data)
+  // }
 }
 </script>
 
